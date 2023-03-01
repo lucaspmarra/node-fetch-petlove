f27d7aaeff05ab2b7e599be7f9faf39336bc9bc2
Lucas Paulino Marra <lucas.marra.terceirizado@tre-go.jus.br>
2023-03-01 13:34:31 -0300

    FEAT: add /users route

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-const fetch = require("node-fetch");
+// const fetch = require("node-fetch");
 const cors = require("cors");
 const express = require("express");
+const axios = require('axios');
 
 const app = express();
 app.use(cors());
 const PORT = 8999;
 
-app.get("/:cep", async function (req, res) {
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Home' });
+});
+
+app.get("/cep/:cep", async function (req, res) {
   try {
-    const response = await fetch(
+    const response = await axios.get(
       `https://viacep.com.br/ws/${req.params.cep}/json/`
     );
-    const { cep, logradouro, localidade, uf } = response.json;
+    const { cep, logradouro, localidade, uf } = await response.json();
+    console.log(cep, logradouro, localidade, uf);
     res.send({
       cep,
       logradouro,
@@ -25,5 +32,18 @@ app.get("/:cep", async function (req, res) {
     });
   }
 });
+
+app.get("/users", async function (req, res) {
+  try {
+    const response = await axios.get(
+      `https://jsm-challenges.s3.amazonaws.com/frontend-challenge.json`
+    )
+    console.log(response);
+    res.send(response);
+  } catch (error) {
+    res.status(404).send({ error: 'Não foi possível realizar a requisição' })
+
+  }
+})
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
